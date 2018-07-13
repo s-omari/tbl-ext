dc1da50329be54ba919e89ba7d5ae690887711eb
Saleh <omari@Salehs-MacBook-Pro.local>
2018-07-12 21:52:45 -0400

    d

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var delimiter = '~';
+
+var matomoServer = '//52.60.221.88/';
+var matomoTMContainerId = 'pFut8LiT';
+var transmitter = 'matomoTagManager';
+// const transmitter = 'matomoTagManager';
+
+
+var matomoConfig = {
+  'matomoServer': '//52.60.221.88/piwik/',
+  'matomoTMContainerId': 'pFut8LiT'
+};
